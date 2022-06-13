e6a13f90ddf266a3842c0e74565a6046b4b28989
yuuicchan0912 <yuuicchan0912@gmail.com>
2022-06-13 19:17:13 +0900

    itemTypeを変えてconstant.jsに値を追加すればガチャを変更できるように修正(SEO的に良くないのであれば別途対策)

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+// constant.jsで設定した項目名を指定
+const itemType = 'LUNCH';
+const target = CONSTS[itemType];
+
+window.onload = () => {
+  const name = target.name;
+  document.title = `${name}ガチャ`;
+  document.getElementById('main').querySelector("h1").innerText = `${name}を決めてください。`;
+  document.getElementById('result').querySelector("h1").innerText = `あなたの${name}は`;
+}
+
 //最大値・最小値を引数に持つ関数
 const getRandom = (min, max) => {
   return Math.floor( Math.random() * (max + 1 - min) ) + min;
 }
 
 const handleClickButton = () => {
-  const main = document.getElementById('main');
-  const result = document.getElementById('result');
+  document.getElementById('main').style.display = 'none';
+  document.getElementById('result').style.display = 'block';
+
+  const min = 1;
+  const max = target.items.length;
+  const random = getRandom(min, max);
+
   const destination = document.getElementById('destination');
+  const targetPrefecture = target.items.find((prefecture) => prefecture.code === random);
 
-  main.style.display = 'none';
-  result.style.display = 'block';
-
-  const random = getRandom(1, 47);
-  const targetPrefecture = CONSTS.PREFECTURES.find((prefecture) => prefecture.code === random);
   destination.innerText = `${targetPrefecture.name}です！`;
 }
