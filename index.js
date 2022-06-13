91c36c91aa84de7423ea601afbeabfc2e3094e54
yuuicchan0912 <yuuicchan0912@gmail.com>
2022-06-13 17:03:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//最大値・最小値を引数に持つ関数
+function getRandom(min, max) {
+  return Math.floor( Math.random() * (max + 1 - min) ) + min;
+}
+
+function handleClickButton() {
+  const main = document.getElementById('main');
+  const result = document.getElementById('result');
+  const destination = document.getElementById('destination');
+
+  main.style.display = 'none';
+  result.style.display = 'block';
+
+  const random = getRandom(1, 47);
+  const targetPrefecture = CONSTS.PREFECTURES.find((prefecture) => prefecture.code === random);
+  destination.innerText = `${targetPrefecture.name}です！`;
+}
